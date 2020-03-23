1a3b90531f82fb1a9b9f8761d8ea6c6559acac47
jantje <eclipse@baeyens.it>
2020-03-23 23:58:41 +0100

    added BREATHE methods; Didn't find all of the expected

--- covid19_ventilator_vub/covid19_ventilator.h
+++ covid19_ventilator_vub/covid19_ventilator.h
@@ -8,14 +8,12 @@
 #pragma once
 #include "Arduino.h"
 
-
-typedef enum
-{
+typedef enum {
     STATE_STARTING = 0,   //first startup of the machine
     STATE_STOPPED,
     STATE_RUNNING_MODE1,
     STATE_RUNNING_MODE2
-}MACHINE_STATE;
+} MACHINE_STATE;
 #include "pins.h"
 #include "Button.h"
 #include "Screen.h"
@@ -24,11 +22,27 @@ typedef enum
 #include "Logger.h"
 #include "SerialReader.h"
 
+//Some global variabels to track performance
 extern uint32_t loopMillis;
-extern uint32_t loopCounter ;
-extern uint32_t PrefLoopMillis ;
-extern uint16_t maxLoopDuration ;
-extern Stream &SerialOutput ;
+extern uint32_t loopCounter;
+extern uint32_t PrefLoopMillis;
+extern uint16_t maxLoopDuration;
 
+//Just in case we want to redirect info to another serial
+extern Stream &SerialOutput;
 
+//we work in deci internally by using this define we can easily switch to centi
+#define UPLIFT  10
+
+//methods in the beademhalings code
+float BREATHE_CONTROL_getInhalePressure();
+void BREATHE_CONTROL_setInhalePressure(float setting);
+
+void BREATHE_CONTROL_setPointInhalePressure(float setting);
+float BREATHE_CONTROL_getPointInhalePressure();
+
+float BREATHE_getBPM();
+float BREATHE_getPID();
+
+void Stepper_ENABLE(bool en);
 
